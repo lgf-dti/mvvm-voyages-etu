e75fc0b84f9a130c9456477f71c783b2aca6cb84
lgf-dti <david.tissot@ac-grenoble.fr>
2025-09-20 12:18:23 +0200

    feat(ui): mise en place de la navigation avec MainActivity et création des fragments de base

---
+++ app/src/main/java/com/example/mvvm_voyages_etu/ui/kitsvoyage/KitsListFragment.java
@@ -0,0 +1,22 @@
+package com.example.mvvm_voyages_etu.ui.kitsvoyage;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.mvvm_voyages_etu.R;
+
+public class KitsListFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_kits_list, container, false);
+    }
+}
